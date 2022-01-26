a4eca0fe9820ff789c6692ec17ff3772d17f42b4
Jonathan Gilroy-Powell <jonathan.gilroy-powell@and.digital>
2022-01-26 21:01:58 +0000

    Repro repo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace devcon_1
+{
+
+  public static class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
+
+// also fails for top-level statements
+// e.g.
+// Console.WriteLine("Hello, top level statement!");
